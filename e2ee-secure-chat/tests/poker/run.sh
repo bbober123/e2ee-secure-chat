23de9cfcb51e25ad227df7959e4da3dc80b8a75b
bbober123 <161208798+bbober123@users.noreply.github.com>
2026-09-04 20:23:06 +0200

    Delete e2ee-secure-chat directory

--- e2ee-secure-chat/tests/poker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-cp ../../src/js/casino/cards.js src/cards.js
-cp ../../src/js/casino/poker-engine.js src/poker-engine.js
-node poker-unit-test.mjs
-echo
-node poker-simulation.mjs
